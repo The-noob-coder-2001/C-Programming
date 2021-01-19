0ca1716cb86eecb4ab39e53891602bc9f714905c
The-noob-coder-2001 <gourangageeksforgeeks@gmail.com>
2021-01-19 17:40:34 +0530

    pattern commited

---
+++ loops1.c
@@ -0,0 +1,14 @@
+//WACP to print the format using loop :  01 0101 010101 01010101 0101010101 010101010101
+
+#include<stdio.h>
+int main()
+{
+   for (int i = 1; i <= 6; ++i) 
+   {
+      for (int j = 1; j <= i; ++j) 
+      {
+         printf("01");
+      }
+      printf("\n");
+    }
+}
